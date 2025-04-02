14872aa68fb3b1284a81f181166489f0fac64600
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-04-02 19:16:12 -0400

    minor changes in model training script

--- main.py
+++ main.py
@@ -10,19 +10,22 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", type=str, default="./data")
     parser.add_argument("--device", type=str, default="cpu")
+    parser.add_argument("--batch_size", type=int, default=1024)
+    parser.add_argument("--num_epochs", type=int, default=1000)
     args = parser.parse_args()
 
     data = Dataset(args.data_path)
-    train_loader, val_loader = data.train_test_split()
+    train_loader, val_loader = data.train_test_split(
+        num_workers=2, persistent_workers=True
+    )
 
     model = HierarchicalRegressionModel(data)
-    print(model)
-    # TODO: check why parameters are not registered
-    # LightningModule may have problems or there is a bug in the model class
-    print(list(model.parameters()))
 
     trainer = L.Trainer(
-        max_epochs=1000, accelerator="cpu", devices=1, log_every_n_steps=5
+        max_epochs=args.num_epochs,
+        accelerator="auto",
+        devices="auto",
+        log_every_n_steps=1,
     )
     trainer.fit(model, train_loader, val_loader)
 
